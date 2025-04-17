f7e7a4c79c295c687b65e0daf63fedf5efacd193
Oduyale Enoch <oduyaleenoch@gmail.com>
2025-04-17 23:10:34 +0100

    feat: added FxRates

--- src/pages/PageLayout.jsx
+++ src/pages/PageLayout.jsx
@@ -3,7 +3,7 @@ import { Header } from '../components/Header';
 
 export const PageLayout = ({ children }) => {
   return (
-    <div className="min-h-screen bg-[#F9F9F9]">
+    <div className="min-h-screen bg-[#F9F9F9] font-poppins">
       <div className="max-w-[1360px] mx-auto px-4 pt-[60px]">
         <Header />
         <main className='py-6' >
